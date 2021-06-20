509c5b2e6549335819d338b554d18b3238b96b26
minimo <minimo.mobile@gmail.com>
2021-06-20 22:06:26 +0900

    update

---
+++ src/components/GameIcon.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="thumbnail-box" @click="onClick">
+    <img :src="thumbnail" :alt="alternative">
+    <div class="title">
+      {{title}}
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "GameIcon",
+  props: {
+    title: { type: String, default: "タイトルなし" },
+    url: { type: String, default: "" },
+    thumbnail: { type: String, default: "" },
+  },
+  data() {
+    return {
+      alternative: "",
+    };
+  },
+  mounted() {
+
+  },
+  methods: {
+    onClick() {
+
+    }
+  }
+}
+</script>
+
+<style scoped lang="scss">
+  .thumbnail-box {
+    width: 25%;
+    height: 256px;
+    margin: 20px auto;
+    cursor: pointer;
+    .title{
+      position: relative;
+      top: -100px;
+      color: white;
+      background: rgba(0,0,0,0.5);
+      font-size: 1.5rem;
+    }
+    img {
+      width: 100%;
+      object-fit: cover;
+    }
+  }
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,60 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <GameIcon
+        title="Return of Hayabusa"
+        url="http://minimo.github.io/ReturnOfHayabusa/"
+        :thumbnail="require('@/assets/hayabusa.png')"
+    ></GameIcon>
+    <GameIcon
+        title="KumaKuma☆BangBang"
+        url="http://minimo.github.io/KumaBang_origin/"
+        :thumbnail="require('@/assets/kumabang_origin.png')"
+    ></GameIcon>
+    <GameIcon
+        title="PlanetBuster"
+        url="http://minimo.github.io/PlanetBuster/"
+        :thumbnail="require('@/assets/planetbuster.png')"
+    ></GameIcon>
+    <GameIcon
+        title="PlanetBuster 2"
+        url="http://minimo.github.io/PlanetBuster2/"
+        :thumbnail="require('@/assets/planetbuster2.png')"
+    ></GameIcon>
+    <GameIcon
+        title="The stars my destination"
+        url="http://minimo.github.io/TheStarsMyDestinationEx/"
+        :thumbnail="require('@/assets/thestarsmydestination.png')"
+    ></GameIcon>
+    <GameIcon
+        title="ひよこは意外と高く空を飛ぶ"
+        url="http://minimo.github.io/jsstg-2015spring2/"
+        :thumbnail="require('@/assets/kumabang_origin.png')"
+    ></GameIcon>
+    <GameIcon
+        title="PlanetBusterRevision"
+        url="http://minimo.github.io/PlanetBusterRevision/"
+        :thumbnail="require('@/assets/planetbuster2.png')"
+    ></GameIcon>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import GameIcon from "@/components/GameIcon";
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
-  }
+    GameIcon
+  },
 }
 </script>
+
+<style scoped lang="scss">
+.home {
+  width: 100%;
+  text-align: center;
+  //display: flex;
+  //justify-content: space-between;
+}
+</style>
