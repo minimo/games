8c2ea809d3f36ae1e9c4a81b5d748bfca5f58ab1
minimo <minimo.mobile@gmail.com>
2021-06-28 12:25:22 +0900

    リロード対策

--- src/store/index.js
+++ src/store/index.js
@@ -3,6 +3,7 @@ import Vuex from 'vuex'
 import state from "@/store/state";
 import mutations from "@/store/mutations";
 import actions from "@/store/actions";
+import createPersistedState from "vuex-persistedstate"
 
 Vue.use(Vuex)
 
@@ -11,5 +12,6 @@ export default new Vuex.Store({
   mutations,
   actions,
   modules: {
-  }
+  },
+  plugins: [createPersistedState({storage: window.sessionStorage})]
 })
