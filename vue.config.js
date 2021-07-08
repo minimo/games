d1a50c508fe94b9d9f2fd4212695a8c0b148fcfd
minimo <minimo.mobile@gmail.com>
2021-07-08 22:16:43 +0900

    タイトル設定追加

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,10 @@
 module.exports = {
     publicPath: '/games/',
     outputDir: 'docs',
+    pages: {
+        index: {
+            entry: "src/main.js",
+            title: "minimo Games",
+        }
+    }
 }
